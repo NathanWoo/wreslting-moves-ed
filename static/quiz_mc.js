ddd36de04d6cc6dc929047665bd9e7d8c1e99780
nathan <nathanwuyang@gmail.com>
2022-04-27 14:22:26 -0400

    nw - add feedbacks to user answers at mc section

--- static/quiz_mc.js
+++ static/quiz_mc.js
@@ -22,35 +22,54 @@ $(document).ready(function () {
 })
 
 function which_answer() {
+    let chosen_answer_html_id = '';
     if ($('#gw:checked').length > 0) {
         answerChosen = 1;
+        chosen_answer_html_id = '#gw';
     }
     if ($('#dla:checked').length > 0) {
         answerChosen = 2;
+        chosen_answer_html_id = '#dla';
     }
     if ($('#sla:checked').length > 0) {
         answerChosen = 3;
+        chosen_answer_html_id = '#sla';
     }
     if ($('#s:checked').length > 0) {
         answerChosen = 4;
+        chosen_answer_html_id = '#s';
     }
     if ($('#ps:checked').length > 0) {
         answerChosen = 5;
+        chosen_answer_html_id = '#ps';
     }
     if ($('#l:checked').length > 0) {
         answerChosen = 6;
+        chosen_answer_html_id = '#l';
     }
     if ($('#sn:checked').length > 0) {
         answerChosen = 7;
+        chosen_answer_html_id = '#sn';
     }
+    
+    answer_feedback(chosen_answer_html_id, answerChosen, currentVidID);
     console.log(answerChosen)
     console.log(currentVidID)
 
-    answer_info = { "answer_chosen": answerChosen, "correctID": currentVidID }
+    answer_info = {"answer_chosen": answerChosen, "correctID": currentVidID }
     check_answer(answer_info)
 }
 
+function answer_feedback(chosen_answer_html_id, chosen_answer_id, currentVidID){
+    if (chosen_answer_id != currentVidID){
+        $(chosen_answer_html_id + '-feedback').append(" ❌ ")
+    } else {
+        $(chosen_answer_html_id + '-feedback').append(" ✅ ")
+    }
+}
+
 function check_answer(info) {
+    console.log(info)
     $.ajax({
         type: "POST",
         url: "/check_answer",
